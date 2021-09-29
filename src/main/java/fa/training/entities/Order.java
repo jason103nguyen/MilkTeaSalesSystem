802ecf49d0a2876d63c8c5f69b0a20f05594c850
jason103nguyen <jason103nguyen@gmail.com>
2021-09-29 11:01:38 +0700

    Order entities created

--- src/main/java/fa/training/entities/Order.java
+++ src/main/java/fa/training/entities/Order.java
@@ -1,5 +1,78 @@
 package fa.training.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "order_id")
+	private int orderId;
+	
+	@Column(name = "create_date")
+	private LocalDate createDate;
+	
+	@Column(name = "status")
+	private boolean status;
+	
+	@Column(name = "total_price")
+	private double totalPrice;
+
+	public Order() {}
+	
+	public Order(int orderId, LocalDate createDate, boolean status, double totalPrice) {
+		super();
+		this.orderId = orderId;
+		this.createDate = createDate;
+		this.status = status;
+		this.totalPrice = totalPrice;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", createDate=" + createDate + ", status=" + status + ", totalPrice="
+				+ totalPrice + "]";
+	}
+	
 }
