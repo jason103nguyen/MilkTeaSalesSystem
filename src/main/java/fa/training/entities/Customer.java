d15d8754b48099fde2677502a9cfde82d9237c14
jason103nguyen <jason103nguyen@gmail.com>
2021-09-29 10:52:22 +0700

    Customer entities created

--- src/main/java/fa/training/entities/Customer.java
+++ src/main/java/fa/training/entities/Customer.java
@@ -1,5 +1,88 @@
 package fa.training.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Customer")
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "customer_id")
+	private int customerId;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	public Customer() {}
+
+	public Customer(int customerId, String lastName, String firstName, String email, String phone) {
+		super();
+		this.customerId = customerId;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", lastName=" + lastName + ", firstName=" + firstName + ", email="
+				+ email + ", phone=" + phone + "]";
+	}
+	
 }
