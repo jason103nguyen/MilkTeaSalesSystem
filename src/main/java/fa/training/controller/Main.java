53b61319dfb239baf670f9db8753df877716c0ce
jason103nguyen <jason103nguyen@gmail.com>
2021-09-30 21:15:35 +0700

    add second huy

--- src/main/java/fa/training/controller/Main.java
+++ src/main/java/fa/training/controller/Main.java
@@ -7,9 +7,15 @@ import fa.training.entities.Store;
 public class Main {
 
 	public static void main(String[] args) {
-		Store store = new Store();
-		StoreManagement sm = new StoreManagement();
-		sm.createStoreFromExcel();
+//		Store store = new Store();
+//		StoreManagement sm = new StoreManagement();
+//		sm.createStoreFromExcel();
+//		CustomerManagement cm = new CustomerManagement();
+//		cm.createCustomerFromExcel();
+//		OrderManagement om = new OrderManagement();
+//		om.createStoreFromExcel();
+//		MilkTeaManagement mt = new MilkTeaManagement();
+//		mt.createMilkTeaFromExcel();
 	}
 
 }
--- src/main/java/fa/training/readfile/OrderExcel.java
+++ src/main/java/fa/training/readfile/OrderExcel.java
@@ -7,6 +7,65 @@
  */
 package fa.training.readfile;
 
-public class OrderExcel {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import fa.training.entities.Order;
+import fa.training.entities.Store;
+
+public class OrderExcel extends ReadFileExcel{
+	private final int COLUMN_INDEX_CREATE_DATE = 0;
+	private final int COLUMN_INDEX_STATUS = 1;
+	private final int COLUMN_INDEX_TOTAL_PRICE = 2;
+	
+	public List<Order> readExcelOrder (String excel) throws IOException{
+		List<Order> listOrder = new ArrayList<Order>();
+		InputStream inputStream = new FileInputStream(new File(FILE));
+		Workbook wb = getFile(inputStream, excel);
+		//Indexsheet is 2
+		Sheet sheet = wb.getSheetAt(2);
+		Iterator<org.apache.poi.ss.usermodel.Row> iterator = sheet.iterator();
+		while (iterator.hasNext()) {
+			Row nextRow = iterator.next();
+			if(nextRow.getRowNum() == 0) {
+				continue;
+			}
+			Iterator<Cell> cellIterator = (nextRow).cellIterator();
+			Order order = new Order();
+			while(cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+				Object cellValue = getCellValue(cell);
+				if(cellValue == null || cellValue.toString().isEmpty()) {
+					continue;
+				}
+				int columnIndex = cell.getColumnIndex();
+				switch (columnIndex) {
+				case COLUMN_INDEX_CREATE_DATE:
+					order.setCreateDate((LocalDate) getCellValue(cell));
+					break;
+				case COLUMN_INDEX_STATUS:
+					order.setStatus((Boolean) getCellValue(cell));
+					break;
+				case COLUMN_INDEX_TOTAL_PRICE:
+					order.setTotalPrice((Double) getCellValue(cell));
+					break;
+					default:
+						break;
+				}
+			}
+			listOrder.add(order);
+		}
+		return listOrder;
+	}
 }
