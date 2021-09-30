782aa7b3d6bb6f1df829a318aa4c4ab4d3e67d57
jason103nguyen <jason103nguyen@gmail.com>
2021-09-30 19:03:42 +0700

    connect to xlsx

--- src/main/java/fa/training/dao/StoreDAO.java
+++ src/main/java/fa/training/dao/StoreDAO.java
@@ -1,11 +1,15 @@
 package fa.training.dao;
 
+import java.util.List;
+
 import fa.training.entities.Store;
+import fa.training.readfile.ReadFileExcel;
+import fa.training.readfile.StoreExcel;
 
 public class StoreDAO extends GenericDao<Store> {
 
 	public StoreDAO() {
-        super(Store.class);
-    }
-	
+		super(Store.class);
+	}
+
 }
---
+++ src/main/java/fa/training/readfile/MilkTeaExcel.java
@@ -0,0 +1,12 @@
+/*
+ * (C) Copyright 2021 Fresher Academy. All Right Reserved .
+ *
+ * @author pc Pham Tran Gia Huy
+ * @date Sep 30, 2021
+ * @version 1.0
+ */
+package fa.training.readfile;
+
+public class MilkTeaExcel {
+
+}
